3870022f787ef7a9258d77086ef2de6a7bdc9500
Scott Mertz <scott@cyngn.com>
2016-05-25 15:47:42 -0700

    debuggerd: add AICP version to tombstones
    Change-Id: I7822a2e133a2f326ad2d8da8f79b0064344d59bf
    
    debuggerd: Rebrand tombstone header to LineageOS
    
    [harryyoud]: Use new props for 15.1
    [rashed]: Bah... use GetProperty
    
    Change-Id: Idd08c2eb7e395b464b1510742bf52833f465db08

--- debuggerd/libdebuggerd/tombstone.cpp
+++ debuggerd/libdebuggerd/tombstone.cpp
@@ -71,7 +71,9 @@ using namespace std::literals::string_literals;
 static void dump_header_info(log_t* log) {
   auto fingerprint = GetProperty("ro.build.fingerprint", "unknown");
   auto revision = GetProperty("ro.revision", "unknown");
+  auto aicp_version = GetProperty("ro.aicp.version", "unknown");
 
+  _LOG(log, logtype::HEADER, "AICP Version: '%s'\n", aicp_version.c_str());
   _LOG(log, logtype::HEADER, "Build fingerprint: '%s'\n", fingerprint.c_str());
   _LOG(log, logtype::HEADER, "Revision: '%s'\n", revision.c_str());
   _LOG(log, logtype::HEADER, "ABI: '%s'\n", ABI_STRING);
